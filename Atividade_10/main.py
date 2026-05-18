04434bbd3907fab4b17f5308dbef9dbe7288350b
Lucas Praciano Soares <pravlucas@gmail.com>
2026-05-18 12:46:59 -0300

    k

---
+++ Atividade_10/main.py
@@ -0,0 +1,32 @@
+import pygame,sys
+from pygame.locals import QUIT
+
+
+nums = [100,120,130,120,150,100,160,200,190,110,115,125,135,170,130]
+
+num_cat = 4
+num_min = min(nums)
+num_max = max(nums)
+tam_cat = (num_max - num_min) / num_cat
+lista_total = [0] * num_cat
+
+def contabiliza_totais(nums,lista_total):
+    # Para cada número na minha lista
+    for i in range(len(nums)):
+        if nums[i] == num_max:
+            lista_total[-1] += 1
+            continue
+        # Para cada faixa/categoria
+        for i_cat in range(num_cat):
+            # Obtem os limites inferior e superior
+            lim_inf = num_min + i_cat * tam_cat
+            lim_sup = lim_inf + tam_cat
+
+            # Chega em qual faixa/categoria o número está com base nesses limites
+            if lim_inf <= nums[i] < lim_sup:
+                lista_total[i_cat] += 1
+                break
+    return lista_total
+
+print(contabiliza_totais(nums,lista_total))
+
