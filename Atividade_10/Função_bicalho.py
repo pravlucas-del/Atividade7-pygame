7db1829587fdbae9696ca68d6abf437c13242ce1
Lucas Praciano Soares <pravlucas@gmail.com>
2026-05-25 11:13:25 -0300

    Terminado

---
+++ Atividade_10/Função_bicalho.py
@@ -0,0 +1,44 @@
+import pygame,sys
+from pygame.locals import QUIT
+
+
+nums = [100,120,130,120,150,100,160,200,190,110,115,125,135,170,130]
+
+num_cat = 4
+num_min = min(nums)
+num_max = max(nums)
+tam_cat = (num_max - num_min) / num_cat
+lista_total = [0] * num_cat
+
+def contabiliza_totais(nums,lista_total):
+    # Para cada número na minha lista
+    for i in range(len(nums)):
+        if nums[i] == num_max:
+            lista_total[-1] += 1
+            continue
+        # Para cada faixa/categoria
+        for i_cat in range(num_cat):
+            # Obtem os limites inferior e superior
+            lim_inf = num_min + i_cat * tam_cat
+            lim_sup = lim_inf + tam_cat
+
+            # Chega em qual faixa/categoria o número está com base nesses limites
+            if lim_inf <= nums[i] < lim_sup:
+                lista_total[i_cat] += 1
+                break
+    return lista_total
+
+print(contabiliza_totais(nums,lista_total))
+
+def draw(screen):
+    screen_h = screen.get_heith()
+    for i in range (len(lista_total)):
+        x = 100 + i * 50
+        h = 20 * lista_total[i]
+        draw.rect(screen,(255,0,0),(x,screen_h - h,25,h))
+        draw.rect(screen,(255,0,0),(x,0,25,h))
+
+lista_nums = [100,120,130,120,150,100,160,200,190,110,115,125,135,]
+num = int(input("Digite um número"))
+lista_nums.append(num)
+
